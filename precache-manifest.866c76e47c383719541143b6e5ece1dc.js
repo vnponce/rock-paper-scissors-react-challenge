b15d8af626d8fb7a9ffa5be45775fca15ef36e01
Abel Ponce <vnponce8@gmail.com>
2020-05-23 21:29:25 -0500

    Updates

---
+++ precache-manifest.866c76e47c383719541143b6e5ece1dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117182d7106f0b935c0f2f4f2c8cf202",
+    "url": "/rock-paper-scissors-react-challenge/index.html"
+  },
+  {
+    "revision": "b4b7e52242b89ac2cf77",
+    "url": "/rock-paper-scissors-react-challenge/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6bb3a96110f6cf7a3003",
+    "url": "/rock-paper-scissors-react-challenge/static/js/2.b1f86bfd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rock-paper-scissors-react-challenge/static/js/2.b1f86bfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4b7e52242b89ac2cf77",
+    "url": "/rock-paper-scissors-react-challenge/static/js/main.293faca5.chunk.js"
+  },
+  {
+    "revision": "e8a7372667192f36aa27",
+    "url": "/rock-paper-scissors-react-challenge/static/js/runtime-main.21947eca.js"
+  }
+]);
