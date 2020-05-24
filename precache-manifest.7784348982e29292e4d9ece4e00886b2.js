1ae17eee4d78da4682de85311cc46711f579536e
Abel Ponce <vnponce8@gmail.com>
2020-05-24 16:36:42 -0500

    Updates

---
+++ precache-manifest.7784348982e29292e4d9ece4e00886b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "355fa64ee71e71ae4c5fc18a3fe0af5c",
+    "url": "/rock-paper-scissors-react-challenge/index.html"
+  },
+  {
+    "revision": "446b5df7fbf282a87875",
+    "url": "/rock-paper-scissors-react-challenge/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e39f283c45182987d573",
+    "url": "/rock-paper-scissors-react-challenge/static/js/2.e329e084.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rock-paper-scissors-react-challenge/static/js/2.e329e084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "446b5df7fbf282a87875",
+    "url": "/rock-paper-scissors-react-challenge/static/js/main.59d49d41.chunk.js"
+  },
+  {
+    "revision": "e8a7372667192f36aa27",
+    "url": "/rock-paper-scissors-react-challenge/static/js/runtime-main.21947eca.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c98117df89ee2970ecd440edbca50653.js"
+  "/rock-paper-scissors-react-challenge/precache-manifest.7784348982e29292e4d9ece4e00886b2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rock-paper-scissors-react-challenge/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
